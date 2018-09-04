3f47f34777213df2032b2db2369a9be983171f5a
Sanctusz <yohans89@gmail.com>
2018-09-04 13:48:02 +0000

    modified:   max_int.py

--- max_int.py
+++ max_int.py
@@ -3,3 +3,8 @@
     unless user inputs a negative number
     repeat getting input from user.
 '''
+number = input("Input any number: ")
+
+while int(number) >= 0:
+    number = input("Input any number: ")
+print("We got a negative")
