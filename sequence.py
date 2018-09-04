980f38ef896d6e99971871d4c8fdc5af30bbcd2f
Sanctusz <yohans89@gmail.com>
2018-09-04 14:03:20 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,8 @@
+'''
+    print out n number of numbers
+    where the sequence is 1, 2, 3, 6, 11, 20, 37.
+    so it prints out the first 3 numbers and then
+    every subsequent number takes the last 3 numbers
+    that came before it and adds them together.
+'''
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
