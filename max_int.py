8593314b65e7fb7064fc865c5a89b7c8df59977f
Sanctusz <yohans89@gmail.com>
2018-09-04 13:55:37 +0000

    Finished Project

--- max_int.py
+++ max_int.py
@@ -2,9 +2,15 @@
     get input from user (any number)
     unless user inputs a negative number
     repeat getting input from user.
+    if max is lower than current input
+    repleace max int
+    print max int when a negative number is inputted
 '''
-number = input("Input any number: ")
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = num_int
 
-while int(number) >= 0:
-    number = input("Input any number: ")
-print("We got a negative")
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if max_int < num_int:
+        max_int = num_int
+print("The maximum is", max_int)
